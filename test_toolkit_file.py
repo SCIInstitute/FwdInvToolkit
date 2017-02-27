2552d17446a837d6776f9b7c47ffb1da4af5279f
Dan White <dwhite@sci.utah.edu>
2017-02-27 12:55:42 -0700

    Testing xml calls

--- test_toolkit_file.py
+++ test_toolkit_file.py
@@ -3,8 +3,17 @@ import os
 
 toolkit = 'Networks/FwdInvToolkit.toolkit'
 
-assert False
+
 assert os.path.isfile(toolkit)
 
 tree = ET.parse(toolkit)
 root = tree.getroot()
+
+print(root.tag)
+print(root.attrib)
+for child in root:
+    print(child.tag, child.attrib)
+
+
+# remove when done with real code
+assert False
