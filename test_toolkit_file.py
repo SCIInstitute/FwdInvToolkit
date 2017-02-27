61898d522b79dfa3ddbfbc29043442cfd2f30d11
Dan White <dwhite@sci.utah.edu>
2017-02-27 14:26:31 -0700

    Functions

--- test_toolkit_file.py
+++ test_toolkit_file.py
@@ -1,6 +1,23 @@
 import xml.etree.ElementTree as ET
 import os
 
+def verify_network_node(network_xml):
+    assert len(network_xml) == 9 # update when version changes
+    assert network_xml[0].tag == 'networkInfo'
+    assert network_xml[1].tag == 'modulePositions'
+    assert network_xml[2].tag == 'moduleNotes'
+    assert network_xml[3].tag == 'connectionNotes'
+    assert network_xml[4].tag == 'moduleTags'
+    assert network_xml[5].tag == 'disabledModules'
+    assert network_xml[6].tag == 'disabledConnections'
+    assert network_xml[7].tag == 'moduleTagLabels'
+    assert network_xml[8].tag == 'loadTagGroups'
+
+def compare_toolkit_network_versions(toolkit_version, network_file):
+    file = 'Networks/' + network_file
+    assert(os.path.isfile(file))
+    assert ET.tostring(toolkit_version) == ET.parse(file)
+
 def test_toolkit_file():
     toolkit_file = 'Networks/FwdInvToolkit.toolkit'
 
@@ -8,7 +25,6 @@ def test_toolkit_file():
 
     tree = ET.parse(toolkit_file)
     root = tree.getroot()
-
 
     assert root.tag == 'boost_serialization'
     assert len(root) == 1
@@ -36,18 +52,9 @@ def test_toolkit_file():
         assert child[0].tag == 'first'
         assert child[1].tag == 'second'
         network_file = child[0].text
-        assert(os.path.isfile('Networks/' + network_file))
         network_xml = child[1]
-        assert len(network_xml) == 9 # update when version changes
-        assert network_xml[0].tag == 'networkInfo'
-        assert network_xml[1].tag == 'modulePositions'
-        assert network_xml[2].tag == 'moduleNotes'
-        assert network_xml[3].tag == 'connectionNotes'
-        assert network_xml[4].tag == 'moduleTags'
-        assert network_xml[5].tag == 'disabledModules'
-        assert network_xml[6].tag == 'disabledConnections'
-        assert network_xml[7].tag == 'moduleTagLabels'
-        assert network_xml[8].tag == 'loadTagGroups'
+        verify_network_node(network_xml)
+        compare_toolkit_network_versions(network_xml, network_file)
 
     # remove when done with real code
     assert False
