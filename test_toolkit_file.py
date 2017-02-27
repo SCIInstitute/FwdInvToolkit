9fbe3d1b3dfbbfab73c430ac45ff32e726a6f728
Dan White <dwhite@sci.utah.edu>
2017-02-27 16:34:32 -0700

    Try comparing sans attrib

--- test_toolkit_file.py
+++ test_toolkit_file.py
@@ -13,13 +13,21 @@ def verify_network_node(network_xml):
     assert network_xml[7].tag == 'moduleTagLabels'
     assert network_xml[8].tag == 'loadTagGroups'
 
+def elements_equal(e1, e2):
+    if e1.tag != e2.tag: return False
+    if e1.text != e2.text: return False
+    if e1.tail != e2.tail: return False
+    #if e1.attrib != e2.attrib: return False
+    if len(e1) != len(e2): return False
+    return all(elements_equal(c1, c2) for c1, c2 in zip(e1, e2))
+
 def compare_toolkit_network_versions(toolkit_version, network_file):
     file = 'Networks/' + network_file
     assert(os.path.isfile(file))
     actual_file_contents = ET.parse(file).getroot()[0]
     assert len(actual_file_contents) == len(toolkit_version)
     for a, b in zip(actual_file_contents, toolkit_version):
-        assert a == b
+        assert elements_equal(a, b)
 
 def test_toolkit_file():
     toolkit_file = 'Networks/FwdInvToolkit.toolkit'
