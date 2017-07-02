cc56c68f172d1f827da2225386595e1b9f705c87
jessdtate <jess@sci.utah.edu>
2017-07-01 21:28:25 -0600

    resaving toolkit file

--- resave_networks.py
+++ resave_networks.py
@@ -1,6 +1,11 @@
+# script to resave all networks in the toolkit.
+# Runs in SCIRun with the -S flag.
+# usage: ./.SCIRun_test -S resave_networks.py
+
 import os #Import OS to build file paths later
 import os.path
 
+# directory with networks
 net_dir = "Networks/"
 
 files = os.walk(net_dir)
