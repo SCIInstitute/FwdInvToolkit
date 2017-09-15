4a5e635e6c619b8778bf7b701ccabafd05e79729
jessdtate <jess@sci.utah.edu>
2017-09-15 10:49:42 -0600

    updating test file

--- test_toolkit_file.py
+++ test_toolkit_file.py
@@ -65,7 +65,7 @@ def test_toolkit_file():
     # count is first:
     assert networks[0].tag == 'count'
     # update actual network count here:
-    assert networks[0].text == '11'
+    assert networks[0].text == '15'
 
     # item_version next:
     assert networks[1].tag == 'item_version'
